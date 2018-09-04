3a7f4d6913d72a16db7bd5bef9bdc5e2f484c484
Mateusz Bochniak <mat.bochniak@gmail.com>
2018-09-04 16:26:45 +0200

    Improved table generation and editing. Resolved #13

--- projects/powerflows/src/lib/powerflows.module.ts
+++ projects/powerflows/src/lib/powerflows.module.ts
@@ -17,10 +17,12 @@
 import {NgModule} from '@angular/core';
 import {PowerFlowsComponent} from './modeler/powerflows.component';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   declarations: [PowerFlowsComponent],
   exports: [PowerFlowsComponent]
